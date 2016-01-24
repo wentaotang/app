fc86d12d61c122c31783365e45ab5cd9ad4d11ac
wentaotang <wentao_tang@126.com>
2016-01-24 10:33:37 +0800

    update

--- app-web/src/main/java/com/hgcode/web/IndexController.java
+++ app-web/src/main/java/com/hgcode/web/IndexController.java
@@ -1,8 +1,11 @@
 package com.hgcode.web;
 
+import org.springframework.stereotype.Controller;
+
 /**
  * Created by wentao on 16/1/24.
  */
+@Controller
 public class IndexController {
 
     public String index(){
