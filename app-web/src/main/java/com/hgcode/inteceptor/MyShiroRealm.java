d0c8e9d2f68fad3b3232a9a01135076f9bcb10d7
wentaotang <wentao_tang@126.com>
2016-03-03 22:52:11 +0800

    删除不必要的文件

---
+++ app-web/src/main/java/com/hgcode/inteceptor/MyShiroRealm.java
@@ -0,0 +1,23 @@
+package com.hgcode.inteceptor;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
+/**
+ * Created by wentao on 16/3/2.
+ */
+public class MyShiroRealm extends AuthorizingRealm {
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        return null;
+    }
+
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        return null;
+    }
+}
