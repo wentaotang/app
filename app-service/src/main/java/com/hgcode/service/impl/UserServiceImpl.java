bfd8cd9d0d771e6ce3debd430d09351f1a917cf9
wentaotang <wentao_tang@126.com>
2016-05-07 05:51:12 +0800

    'update'

--- app-service/src/main/java/com/hgcode/service/impl/UserServiceImpl.java
+++ app-service/src/main/java/com/hgcode/service/impl/UserServiceImpl.java
@@ -1,12 +1,10 @@
 package com.hgcode.service.impl;
 
-import com.github.pagehelper.PageHelper;
 import com.hgcode.domain.UserEntity;
 import com.hgcode.mybatis.repository.UserRepository;
 import com.hgcode.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import tk.mybatis.mapper.entity.Example;
 
 /**
  * Created by wentao on 2016/1/9.
