242831e6433ad3e7f589fe44c51f86344fb03c1c
wentaotang <=wentao_tang@126.com>
2016-06-27 17:44:15 +0800

    update

---
+++ app-web/src/main/java/com/hgcode/UserContext.java
@@ -0,0 +1,29 @@
+package com.hgcode;
+
+import com.hgcode.domain.UserEntity;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: wentao_tang
+ * Date: 2016/6/27
+ * Time: 17:41
+ * To change this template use File | Settings | File Templates.
+ */
+public class UserContext implements AutoCloseable {
+
+    static final ThreadLocal<UserEntity> current = new ThreadLocal<UserEntity>();
+
+    public UserContext(UserEntity user) {
+        current.set(user);
+    }
+
+    public static UserEntity getCurrentUser() {
+        return current.get();
+    }
+
+
+    @Override
+    public void close() throws Exception {
+        current.remove();
+    }
+}
