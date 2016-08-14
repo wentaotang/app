d967e8bfcad059002b5b9544a580ac5619d9fc2a
wentaotang <wentao_tang@126.com>
2016-08-14 21:26:27 +0800

    udpate

--- app-weixin/src/main/java/com/hgcode/weixin/share/response/WeixinResponse.java
+++ app-weixin/src/main/java/com/hgcode/weixin/share/response/WeixinResponse.java
@@ -4,8 +4,13 @@ package com.hgcode.weixin.share.response;
  * Created by wentao on 16/8/14.
  */
 public abstract class WeixinResponse {
-
+    /**
+     * 错误编码
+     */
     private Integer errcode;
+    /**
+     * 错误信息
+     */
     private String errmsg;
 
     public Integer getErrcode() {
