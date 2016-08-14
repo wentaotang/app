89c70cc3864271ac5b6e0d8a7ed1b0ce5aa933fa
wentaotang <wentao_tang@126.com>
2016-08-14 22:45:22 +0800

    update

--- app-weixin/src/main/java/com/hgcode/weixin/share/util/WeixinUtil.java
+++ app-weixin/src/main/java/com/hgcode/weixin/share/util/WeixinUtil.java
@@ -7,6 +7,7 @@ import com.hgcode.weixin.share.response.WeiXinUserInfoResponse;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -26,58 +27,47 @@ public class WeixinUtil {
      */
     public static AccessTokenResponse getAccessToken(String appid) {
         String url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=" + appid + "&secret=APPSECRET";
-        OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder().url(url).build();
-        Response response = null;
-        try {
-            response = client.newCall(request).execute();
-            return (AccessTokenResponse) JSON.parse(response.body().string());
-        } catch (IOException e) {
-            log.error("获取微信access_token异常",e);
+        String result=buildResponse(url);
+        if(StringUtils.isBlank(result))
             return null;
-        }
+        return (AccessTokenResponse) JSON.parse(buildResponse(result));
     }
 
     public static WebAccessTokenResponse getWebAccessToken(String appid, String secret, String code){
         String url ="https://api.weixin.qq.com/sns/oauth2/access_token?appid="+appid+"&secret="+secret+"&code="+code+"&grant_type=authorization_code";
-        OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder().url(url).build();
-        Response response = null;
-        try {
-            response = client.newCall(request).execute();
-            return (WebAccessTokenResponse) JSON.parse(response.body().string());
-        } catch (IOException e) {
-            log.error("获取web access_token异常",e);
+        String result=buildResponse(url);
+        if(StringUtils.isBlank(result))
             return null;
-        }
+        return (WebAccessTokenResponse) JSON.parse(buildResponse(result));
     }
 
     public static WebAccessTokenResponse refreshWebAccessToken(String appid,String refreshToken){
         String url="https://api.weixin.qq.com/sns/oauth2/refresh_token?appid="+appid+"&grant_type=refresh_token&refresh_token="+refreshToken;
-        OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder().url(url).build();
-        Response response = null;
-        try {
-            response = client.newCall(request).execute();
-            return (WebAccessTokenResponse) JSON.parse(response.body().string());
-        } catch (IOException e) {
-            log.error("refresh微信access_token异常",e);
+        String result=buildResponse(url);
+        if(StringUtils.isBlank(result))
             return null;
-        }
+        return (WebAccessTokenResponse) JSON.parse(buildResponse(result));
     }
 
-    public static WeiXinUserInfoResponse getUserInfo(String access_token, String openid){
-        String url="https://api.weixin.qq.com/sns/userinfo?access_token="+access_token+"&openid="+openid+"&lang=zh_CN ";
-        OkHttpClient client = new OkHttpClient();
+    public static WeiXinUserInfoResponse getUserInfo(String access_token, String openid) {
+        String url = "https://api.weixin.qq.com/sns/userinfo?access_token=" + access_token + "&openid=" + openid + "&lang=zh_CN ";
+        String result=buildResponse(url);
+        if(StringUtils.isBlank(result))
+            return null;
+        return (WeiXinUserInfoResponse) JSON.parse(buildResponse(result));
+    }
+    private static  String buildResponse(String url){
         Request request = new Request.Builder().url(url).build();
+        final OkHttpClient client = new OkHttpClient();
         Response response = null;
         try {
             response = client.newCall(request).execute();
-            return (WeiXinUserInfoResponse) JSON.parse(response.body().string());
+            return response.body().string();
         } catch (IOException e) {
-            log.error("refresh微信access_token异常",e);
-            return null;
+            log.error("okhttp处理异常{}",url, e);
+            return "";
         }
+    }
     }
 
 }
