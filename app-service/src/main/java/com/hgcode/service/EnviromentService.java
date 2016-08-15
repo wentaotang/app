183d8e0642c96fa7a2b2971caef02940df08e42f
wentaotang <=wentao_tang@126.com>
2016-08-15 17:05:36 +0800

    整合maven的profile 和 filter

---
+++ app-service/src/main/java/com/hgcode/service/EnviromentService.java
@@ -0,0 +1,15 @@
+package com.hgcode.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: wentao_tang
+ * Date: 2016/8/4
+ * Time: 16:57
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+public class EnviromentService {
+
+}
