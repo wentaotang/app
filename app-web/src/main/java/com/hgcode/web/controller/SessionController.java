727c34bdacbd146b8594434ff9199be4cdbe3bc1
wentaotang <=wentao_tang@126.com>
2016-10-24 17:35:18 +0800

    update pom

---
+++ app-web/src/main/java/com/hgcode/web/controller/SessionController.java
@@ -0,0 +1,12 @@
+package com.hgcode.web.controller;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: wentao_tang
+ * Date: 2016/10/24
+ * Time: 17:10
+ * To change this template use File | Settings | File Templates.
+ */
+
+public class SessionController {
+}
