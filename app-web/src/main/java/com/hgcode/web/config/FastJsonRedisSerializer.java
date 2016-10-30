5ab248aad1fa97745c048caa0ef7cad2b0822d5e
wentaotang <wentao_tang@126.com>
2016-10-30 17:04:51 +0800

    新增spring-session config

---
+++ app-web/src/main/java/com/hgcode/web/config/FastJsonRedisSerializer.java
@@ -0,0 +1,24 @@
+package com.hgcode.web.config;
+
+import com.alibaba.fastjson.JSON;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.SerializationException;
+
+/**
+ * Created by wentao on 2016/10/30.
+ */
+public class FastJsonRedisSerializer implements RedisSerializer<Object> {
+    @Override
+    public byte[] serialize(Object o) throws SerializationException {
+        if (o == null)
+            return new byte[0];
+        return JSON.toJSONBytes(o);
+    }
+
+    @Override
+    public Object deserialize(byte[] bytes) throws SerializationException {
+        if (bytes == null || bytes.length == 0)
+            return null;
+        return JSON.parse(bytes);
+    }
+}
--- app-web/src/main/java/com/hgcode/web/controller/IndexController.java
+++ app-web/src/main/java/com/hgcode/web/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.hgcode.web.controller;
 
 import com.hgcode.domain.UserEntity;
 import com.hgcode.service.UserService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
@@ -9,6 +10,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.List;
 
@@ -33,5 +35,15 @@ public class IndexController {
             return bindingResult.getAllErrors();
         }
         return null;
+    }
+
+    @RequestMapping("/session")
+    public String session(HttpServletRequest request){
+        String param=request.getParameter("param");
+        if(StringUtils.isBlank(param)){
+            request.getSession().setAttribute("param","123456");
+        }
+        request.getSession().setAttribute("param",param);
+        return (String)request.getSession().getAttribute("param");
     }
 }
