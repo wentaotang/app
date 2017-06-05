6b106f7e3290e23281b9fd51bc85f62f5b53fa9c
wentaotang <wentao_tang@126.com>
2017-06-06 07:01:12 +0800

    upate

---
+++ app-web/src/main/java/com/hgcode/web/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.hgcode.web.controller;
+
+import com.hgcode.web.support.Menu;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * Created by wentao on 2016/12/25.
+ */
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @RequestMapping("/list")
+    public String  list(){
+        return "user/list";
+    }
+
+}
